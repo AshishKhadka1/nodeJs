feca01f63e93871935f9cbc6ff0d86614228fa9a
Ashish khadka <khadkaashish50@gmail.com>
2024-01-12 20:26:37 +0545

    Remove the now ignored directory node_modules

---
+++ DeltaCourseNodeJs/math.js
@@ -0,0 +1,3 @@
+const sum = (a,b) => a+b; 
+const mul = (a,b) => a*b;
+const g = 9.8;
---
+++ DeltaCourseNodeJs/script.js
@@ -0,0 +1,7 @@
+// let n = 5
+
+// for(let i=0; i<10; i++){
+//     console.log("hello", i);
+// }
+
+console.log(process.argv);
